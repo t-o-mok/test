483d0fca4f3461cabda247b3404da86d9ba57e11
t-o-mok <38946515+t-o-mok@users.noreply.github.com>
2021-07-25 14:18:22 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
-motor.servo(motor.Servos.S1, 90)
+function 角度設定 (サーボ: number, 角度: number) {
+    if (1 == サーボ) {
+        motor.servo(motor.Servos.S1, 角度)
+    } else if (2 == サーボ) {
+        motor.servo(motor.Servos.S2, 角度)
+    } else if (3 == サーボ) {
+        motor.servo(motor.Servos.S3, 角度)
+    } else if (4 == サーボ) {
+        motor.servo(motor.Servos.S4, 角度)
+    } else {
+    	
+    }
+}
+let サーボ = 1
+let 角度 = 90
+let 増分 = 1
 basic.forever(function () {
-	
+    basic.pause(1000)
+    basic.pause(1000)
+    basic.pause(1000)
 })
