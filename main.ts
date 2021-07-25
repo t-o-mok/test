ded9e8026d1b96651ca03ae98a8544eeedf5b503
t-o-mok <38946515+t-o-mok@users.noreply.github.com>
2021-07-25 15:46:01 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -25,9 +25,8 @@ function 角度設定 (サーボ: number, 角度: number) {
     }
 }
 input.onButtonPressed(Button.A, function () {
-    角度設定(サーボ, 増分)
+    角度設定(サーボ, 1)
     サーボ出力(サーボ)
-    角度表示(サーボ)
 })
 function サーボ出力 (サーボ: number) {
     if (1 == サーボ) {
@@ -43,28 +42,31 @@ function サーボ出力 (サーボ: number) {
     }
 }
 input.onButtonPressed(Button.AB, function () {
+    角度表示(サーボ)
+    basic.clearScreen()
+    角度表示(サーボ)
+    basic.clearScreen()
     if (3 >= サーボ) {
         サーボ += 1
     } else {
         サーボ = 1
     }
-    角度表示(サーボ)
 })
 input.onButtonPressed(Button.B, function () {
-    角度設定(サーボ, -1 * 増分)
+    角度設定(サーボ, -1)
     サーボ出力(サーボ)
-    角度表示(サーボ)
 })
-let 増分 = 0
+let サーボ = 0
 let 角度4 = 0
 let 角度3 = 0
 let 角度2 = 0
 let 角度1 = 0
-let サーボ = 0
-サーボ = 1
 角度1 = 90
 角度2 = 90
 角度3 = 90
 角度4 = 90
-増分 = 1
-角度表示(サーボ)
+サーボ出力(1)
+サーボ出力(2)
+サーボ出力(3)
+サーボ出力(4)
+サーボ = 1
